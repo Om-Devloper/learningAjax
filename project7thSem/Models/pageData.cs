7d7a9abf8b54239fe694fcc022a08e16521cf534
Om <pandyaom122@gmail.com>
2023-08-12 12:24:52 +0530

    1. designing updates 2. allow email one process start

--- project7thSem/Models/pageData.cs
+++ project7thSem/Models/pageData.cs
@@ -22,6 +22,7 @@
         public int SectorID { get; set; }
         public string ContactPerson { get; set; }
         public string ContactPhone { get; set; }
+        public string poliRegionName  { get; set; }
 
 
     }
